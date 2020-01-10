d71d74a71ba11cf3599b0d3d012bc86593558b52
Salvador Murillo <chava-murillo@users.noreply.github.com>
2020-01-09 21:03:20 -0600

    example js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function myApp(a){
+    
+    if (a == "4"){
+        console.log("El numero es 4");
+    }
+    else {
+        console.log("El numero no es 4");
+    }        
+}
+
+var a = 4;
+myApp();
